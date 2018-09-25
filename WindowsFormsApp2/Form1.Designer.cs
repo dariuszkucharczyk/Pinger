f9cfd11e1d99a58decd2b0e07dcec9b04139f72c
dariuszkucharczyk <dariuszkucharczyk@o2.pl>
2018-09-25 15:46:28 +0200

    First commit

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(277, 371);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(199, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Wysyłaj zapytanie PING";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.Black;
+            this.richTextBox1.DetectUrls = false;
+            this.richTextBox1.ForeColor = System.Drawing.Color.Lime;
+            this.richTextBox1.HideSelection = false;
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.ReadOnly = true;
+            this.richTextBox1.Size = new System.Drawing.Size(464, 281);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 387);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(259, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 371);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(132, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Adres do testowania PING";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(488, 431);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Pinger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.IO;
+using System.Net;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+
+       
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        delegate void dodawanie();
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Graphics obj;
+            obj = this.CreateGraphics();
+            Pen myPen = new Pen(Color.Yellow, 20);
+            obj.DrawLine(myPen, 0, 340, 500, 340);
+
+
+            string command = "/c ping " + textBox1.Text;
+                ProcessStartInfo jakiProces = new ProcessStartInfo("CMD", command);
+                Process proc = new Process();
+                proc.StartInfo = jakiProces;
+                jakiProces.RedirectStandardOutput = true;
+                jakiProces.UseShellExecute = false;
+                jakiProces.CreateNoWindow = true;
+               
+                proc.OutputDataReceived += new DataReceivedEventHandler(linia);
+
+                proc.Start();
+                proc.BeginOutputReadLine();
+
+        }
+
+        public void linia(object sender, DataReceivedEventArgs e)
+        {
+            if ((e.Data != null ) )
+            {
+                string nowalinia = e.Data.Trim() + Environment.NewLine;
+               
+                void Dodawan()
+                {
+                    richTextBox1.Text += nowalinia;
+                }
+
+                dodawanie dod = new dodawanie(Dodawan);
+                richTextBox1.Invoke(dod);
+
+            }
+        }
+    }
+    
+}
